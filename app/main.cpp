c77de04d50a8c7ad92f3ed614d684c8ffe770b00
Iuri Matias <iuri.matias@gmail.com>
2026-01-28 09:56:32 -0500

    flush output to avoid github actions timing out when running tests
    test
    
    test

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,42 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <cstdio>
 #include "logos_core.h"
 #include "command_line_parser.h"
 #include "plugin_manager.h"
 
+// Custom message handler that ensures immediate flushing of output
+// We do this due to github actions timing out
+void flushingMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    QByteArray localMsg = msg.toLocal8Bit();
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug: %s\n", localMsg.constData());
+        break;
+    case QtInfoMsg:
+        fprintf(stderr, "Info: %s\n", localMsg.constData());
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s\n", localMsg.constData());
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        fflush(stderr);
+        abort();
+    }
+    fflush(stderr);  // Flush after every message
+}
+
 int main(int argc, char *argv[]) {
+    // Install custom message handler before any Qt operations
+    qInstallMessageHandler(flushingMessageHandler);
+
     QCoreApplication app(argc, argv);
     app.setApplicationName("logoscore");
     app.setApplicationVersion("1.0");
