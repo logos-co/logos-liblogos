351149408ab977f41cbb8e855ad30944308e0579
Iuri Matias <iuri.matias@gmail.com>
2026-01-28 11:26:10 -0500

    support --call arg to call module methods in the cmd line
    support --call arg to call module methods in the cmd line
    
    cleanup
    
    cleanup

--- app/command_line_parser.h
+++ app/command_line_parser.h
@@ -3,13 +3,21 @@
 
 #include <QString>
 #include <QStringList>
+#include <QList>
 
 class QCoreApplication;
+
+struct ModuleCall {
+    QString moduleName;
+    QString methodName;
+    QStringList params;  // Raw param strings (including @file refs)
+};
 
 struct CoreArgs {
     bool valid;
     QString modulesDir;           // Optional: custom modules directory
     QStringList loadModules;      // Optional: modules to load in order
+    QList<ModuleCall> calls;      // Optional: module method calls to execute
 };
 
 CoreArgs parseCommandLineArgs(QCoreApplication& app);
